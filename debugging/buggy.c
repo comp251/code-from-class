118e7de3c0ddf8f66adc20d49b8b56caa2ae6f02
Marion Lang <langm@rhodes.edu>
2023-02-15 12:53:14 -0600

    buggy code from 2/15

---
+++ debugging/buggy.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_WORDS 80
+#define MAX_LINE 800
+
+void tokenize(char *input, char *words[], int maxlen);
+
+void reverse_words(char *words[]) {
+  // compute length of words; swap words[i] with words[length-1-i]
+  // MY BUG!
+  // int wordslen = strnlen(words[0], MAX_LINE);
+  int wordslen = 0;
+  while (words[wordslen] != NULL) {
+    wordslen++;
+  }
+  printf("wordslen = %d\n", wordslen);
+  for (int i = 0; i < wordslen; i++) {
+    printf("words[%d]: %s\n", i, words[i]);
+    // swap? stop condition
+    words[i] = words[wordslen - 1 - i];
+    words[wordslen - 1 - i] = words[i];
+  }
+}
+
+int main(int argc, char **argv) {
+  char *words[MAX_WORDS];
+  char input_buffer[MAX_LINE];
+  // read a line
+  printf("> ");
+  char *ret = fgets(input_buffer, MAX_LINE, stdin);
+  while (ret != NULL) {
+    // while we have input, tokenize the line and then reverse the individual
+    // words
+    tokenize(input_buffer, words, MAX_WORDS);
+    reverse_words(words);
+    // print the reversed words
+    for (int i = 0; words[i] != NULL; i++) {
+      printf("%s ", words[i]);
+    }
+    printf("\n");
+    // read next line
+    printf("> ");
+    ret = fgets(input_buffer, MAX_LINE, stdin);
+  }
+}
+
+void tokenize(char input[], char *words[], int maxlen) {
+  memset(words, 0, sizeof(char *) * maxlen);
+  int c = 0;
+  words[c] = strtok(input, " ");
+  while (words[c] != NULL && c < maxlen) {
+    c++;
+    words[c] = strtok(NULL, " ");
+  }
+  words[maxlen - 1] = NULL;
+}
